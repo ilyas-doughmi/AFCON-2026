49b1374f47e4479f7c6f2862a2a9d2aa9dcb228c
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-04-02 14:45:05 +0100

    feat: add index page with AFCON 2026 quarter finals template and match fetching

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        AFCON 2026 - Quarter Finals
+    </div>
+
+</template>
+
+
+<script setup>
+
+import { useMatchStore } from '~/stores/matchStore';
+
+const matches = useMatchStore()
+
+matches.fetchMatches();
+
+</script>
